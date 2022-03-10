bb43afb2c8e47c940abd00d6464c23bdd01ed15f
sanjay <ssah6694@gmail.com>
2022-03-11 00:39:36 +0530

    [sanjay]Add: added Leap year pro

---
+++ LeapYear/LeapYear/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    internal class Class1
+    {
+        public static bool isLeapYear(int year)
+        {
+            if ((year % 400 == 0) || (year % 100 != 0 && year % 4 == 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool isleap(bool isleapyear, int year)
+        {
+            if (isleapyear)
+            {
+                Console.WriteLine(year + " is a leap year");
+            }
+            else
+            {
+                Console.WriteLine(year + "  is not a leap year");
+            }
+            return isleapyear;
+
+        }
+
+    }
+}
---
+++ LeapYear/LeapYear/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LeapYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter year");
+            int year = int.Parse(Console.ReadLine());
+            bool isleapyear = Class1.isLeapYear(year);
+            Class1.isleap(isleapyear, year);
+        }
+    }
+}
